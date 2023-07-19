cd642834fb61bda3fcb19021448f23533fc033b8
Silverthg <fmosorkin@gmail.com>
2023-07-19 20:48:24 +0900

    Added react app, some pages and store

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
+import RegisterPage from "pages/RegisterPage";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route exact path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
+export default App;
